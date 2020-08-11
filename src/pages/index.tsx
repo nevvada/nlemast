7d7d5315030cc864a00a0c7496348fc0db41e850
nlemast <nlemaster47@gmail.com>
2020-08-10 21:25:31 -0400

    Import Home component into App

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Home from '../components/Home/Home';
 import Layout from '../components/Layout/Layout';
 
 const App = () => {
 	return (
 		<Layout>
-			henlo world
+			<Home />
 		</Layout>
 	)
 };
