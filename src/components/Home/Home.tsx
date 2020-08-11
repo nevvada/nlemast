ca6c247735cdd9131a084b335ed952621a052b40
nlemast <nlemaster47@gmail.com>
2020-08-10 21:25:10 -0400

    Create Home component with styled component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { StyledHeader } from './styles';
+
+const Home = () => {
+	return (
+		<StyledHeader>software engineer | writer | very online person</StyledHeader>
+	)
+};
+
+export default Home;
