d19bedec19197e99bf7a3a0c394dc386d149f3bf
nlemast <nlemaster47@gmail.com>
2020-08-15 12:31:04 -0400

    Import Code component and replace pre value with rendering new component with JS as default language

--- root-wrapper.js
+++ root-wrapper.js
@@ -3,51 +3,22 @@ import { MDXProvider } from '@mdx-js/react';
 import React from 'react';
 import theme from 'prism-react-renderer/themes/github';
 
+import Code from './src/components/Code/Code';
+
 const components = {
 	'p.inlineCode': props => (
 		<code style={{ backgroundColor: 'lightgray' }} {...props} />
 	),
-    pre: (props) => {
-		console.log('oi', props.children.props)
-		const className = props.children.props.className || '';
-		const matches = className.match(/language-(?<lang>.*)/);
-
-		return (
-			<Highlight
-				{...defaultProps}
-				code={props.children.props.children.trim()}
-				language={
-					matches && matches.groups && matches.groups.lang
-						? matches.group.lang
-						: ''
-				}
-				theme={theme}
-			>
-				{({
-					className,
-					style,
-					tokens,
-					getLineProps,
-					getTokenProps,
-				}) => {
-					return (
-						<pre className={className} style={style}>
-							{tokens.map((line, index) => {
-								return (
-									<div {...getLineProps({ line, key: index })}>
-										{line.map((token, key) => {
-											return (
-												<span {...getTokenProps({ token, key })} />
-											)
-										})}
-									</div>
-								)
-							})}
-						</pre>
-					)
-				}}
-			</Highlight>
-		)
+    pre: ({ children: { props } }) => {
+		if (props.mdxType === 'code') {
+			return (
+				<Code 
+					codeString={props.children.trim()}
+					language="javascript"
+					{...props}
+				/>
+			)
+		}
 	},
 };
 
