a22fa3a39c36da59517dfa16a5d988ad7da865d2
nlemast <nlemaster47@gmail.com>
2020-08-15 12:32:43 -0400

    Remove unused imports

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,7 +1,5 @@
-import Highlight, { defaultProps } from 'prism-react-renderer';
 import { MDXProvider } from '@mdx-js/react';
 import React from 'react';
-import theme from 'prism-react-renderer/themes/github';
 
 import Code from './src/components/Code/Code';
 
