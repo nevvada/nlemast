de7c10345d559f001f79720ee3583920a21f4795
nlemast <nlemaster47@gmail.com>
2020-08-16 18:29:29 -0400

    Import SEO component and useSiteMetadata hook; pass as prop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import SEO from 'react-seo-component';
 
 import Home from '../components/Home/Home';
 import Layout from '../components/Layout/Layout';
 
+import useSiteMetadata from '../hooks/useSiteMetadata';
+
 const App = () => {
+	const {
+		description,
+		siteLanguage,
+		siteLocale,
+		siteUrl,
+		title
+	} = useSiteMetadata();
+
 	return (
 		<Layout>
+			<SEO
+				description={description || `nothing`}
+				pathname={siteUrl}
+				siteLanguage={siteLanguage}
+				siteLocale={siteLocale}
+				title={title}
+			/>
 			<Home />
 		</Layout>
 	)
