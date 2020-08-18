35601ccf2b267d443881fedeb5eb544a795a014b
nlemast <nlemaster47@gmail.com>
2020-08-17 21:49:55 -0400

    Move React SEO component to separate file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
-import SEO from 'react-seo-component';
 
 import Home from '../components/Home/Home';
 import Layout from '../components/Layout/Layout';
-
-import useSiteMetadata from '../hooks/useSiteMetadata';
+import SEO from '../components/SEO/SEO';
 
 const App = () => {
-	const {
-		description,
-		siteLanguage,
-		siteLocale,
-		siteUrl,
-		title
-	} = useSiteMetadata();
-
 	return (
 		<Layout>
-			<SEO
-				description={description || `nothing`}
-				pathname={siteUrl}
-				siteLanguage={siteLanguage}
-				siteLocale={siteLocale}
-				title={title}
-			/>
+			<SEO />
 			<Home />
 		</Layout>
 	)
