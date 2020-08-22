d73a0d32d5e4bdc64fcc1fce573016d17d0f17db
nlemast <nlemaster47@gmail.com>
2020-08-22 12:34:25 -0400

    Import and invoke useSiteMetadata to pass in blog post metadata into SEO component

--- src/templates/blogPostTemplate.tsx
+++ src/templates/blogPostTemplate.tsx
@@ -3,13 +3,23 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 import React from 'react';
 
 import Layout from '../components/Layout/Layout';
+import SEO from '../components/SEO/SEO';
+
 import { StyledBlogPost, StyledTitle } from '../components/BlogPost/styles';
 
+import useSiteMetadata from '../hooks/useSiteMetadata';
+
 const BlogPostTemplate = ({ data }) => {
-    const { body, frontmatter } = data.mdx;
+    const { body, excerpt, frontmatter } = data.mdx;
+    const { siteLanguage = '' } = useSiteMetadata();
 
     return (
         <Layout>
+            <SEO
+                description={excerpt}
+                siteLanguage={siteLanguage}
+                title={frontmatter.title}
+            />
             <StyledBlogPost>
                 <StyledTitle>{frontmatter.title}</StyledTitle>
                 <p>{frontmatter.date}</p>
@@ -24,6 +34,7 @@ export const query = graphql`
     query PostsBySlug($slug: String!) {
         mdx(fields: { slug: { eq: $slug } }) {
             body
+            excerpt(pruneLength: 200)
             frontmatter {
                 date(formatString: "YYYY MMMM Do")
                 title
