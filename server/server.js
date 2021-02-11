b6aac76b6bc95b01e481fc10e063c76d550cae28
nlemast <nlemaster47@gmail.com>
2021-02-11 13:24:43 -0500

    Create simple express server and delete old gatsby commands

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('./dist', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/bundle.js'));
+});
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .type('html')
+    .sendFile(path.join(__dirname), '../src/index.html');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
+});
