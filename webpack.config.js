53c92f5d1b2255bac134c74f5c7fdc1ec61d3a32
nlemast <nlemaster47@gmail.com>
2021-02-12 11:57:03 -0500

    Initial attempt at getting webpack configured to run from bundle for server

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const environment = process.env.NODE_ENV || 'development';
 
 module.exports = {
-    mode: environment,
-    entry: path.join(__dirname, 'src', 'index.tsx'),
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
-                },
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['*', '.ts', '.tsx', '.js', '.jsx'],
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'src'),
-        port: 8080,
-        publicPath: '/dist',
-        hot: true,
-        historyApiFallback: true,
-    },
+  mode: environment,
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  // target: 'node',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    port: 8000,
+    publicPath: '/dist',
+    hot: true,
+    historyApiFallback: true, // for react router
+  },
 };
