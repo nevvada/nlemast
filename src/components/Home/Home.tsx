766b98ebd44d568335b5056086d86f0d975f3443
nlemast <nlemaster47@gmail.com>
2021-02-15 11:24:38 -0500

    Create styles file with global reset and styled header

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { StyledGlobal, StyledHeader } from './styles';
 
 const Home: React.FC = () => (
   <>
+    <StyledGlobal />
+    <StyledHeader>
+      <h1>nathan le master</h1>
+      <h2>software engineer, writer, very online person</h2>
+    </StyledHeader>
+    <br />
   </>
 );
 
