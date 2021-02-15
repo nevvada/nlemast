7707b632809fedcac7449543380c1ca8be828115
nlemast <nlemaster47@gmail.com>
2021-02-15 11:23:48 -0500

    Replace App with Home, and update module resolution to node in tsconfig

--- src/components/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => (
-  <div>hi</div>
-);
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+import Home from './components/Home/Home';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<Home />, document.querySelector('#root'));
