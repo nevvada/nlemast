3bc5bcd39d6f3cd181b5150e6e0fc43274ecf050
nlemast <nlemaster47@gmail.com>
2021-02-16 16:46:29 -0500

    Remove Contact and Header components, replace with About

---
+++ src/components/About/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About: React.FC = () => (
+  <>
+    <p>
+      Hey, I'm Nathan. I'm a software engineer based out of Brooklyn, New York, by way of California and Arkansas.
+      I'm most interested in futurism, internet rabbit holes, urban planning, history, and most recently, cycling.
+      I wrote a lot of papers in college, so I writing is a bit of a passion of mine. I'll probably throw up some things I've written about here,
+      some programming-related, others random stuff I've burned time researching or learning about.
+      <br />
+      <br />
+      You can find me on Github or LinkedIn.
+    </p>
+  </>
+);
+
+export default About;
