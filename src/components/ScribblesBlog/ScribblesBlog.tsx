e89ad24ac940b559caf54cf0ee45636ee50a6c87
nlemast <nlemaster47@gmail.com>
2021-02-15 19:54:30 -0500

    Remame components to better match route paths

---
+++ src/components/ScribblesBlog/ScribblesBlog.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Scribbles: React.FC = () => (
+  <>
+    <Header />
+    random stuff blog
+  </>
+);
+
+export default Scribbles;
