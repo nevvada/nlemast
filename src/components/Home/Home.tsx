f4fd02a1b7f5cc0c7890ff2f02032abec146a784
nlemast <nlemaster47@gmail.com>
2021-02-17 17:41:01 -0500

    Remove unused CodingBlog file and clean up comments in Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,13 +3,8 @@ import { Link } from 'react-router-dom';
 
 import { StyledH1, StyledH2, StyledHeader, StyledNav } from './styles';
 
-// import GithubLogo from '../../images/GithubLogo.svg';
-// import LinkedInLogo from '../../images/LinkedInLogo.svg';
-
 const Home: React.FC = () => (
   <>
-    {/* <img alt="github logo" src={GithubLogo} /> */}
-    {/* <img alt="linkedin logo" src={LinkedInLogo} /> */}
     <StyledHeader>
       <StyledH1><Link to="/">nathan le master</Link></StyledH1>
       <StyledH2>software engineer &middot; writer &middot; very online person</StyledH2>
