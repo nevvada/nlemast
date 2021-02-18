80d8e9a494db2764248076267a73bc28040ce064
nlemast <nlemaster47@gmail.com>
2021-02-18 12:20:37 -0500

    Connect BlogPost to context to access posts from blog type params; create fallback for bad routes

--- src/components/BlogPost/BlogPost.tsx
+++ src/components/BlogPost/BlogPost.tsx
@@ -1,33 +1,42 @@
 import Markdown from 'markdown-to-jsx';
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Anchor from '../Anchor/Anchor';
 import Image from '../Image/Image';
 
+import PostsContext from '../../PostsContext';
+
 import { StyledArticle, StyledH4 } from './styles';
 
 const BlogPost: React.FC = () => {
-  const { state } = useLocation<Post>();
-  const { date, markdown, title } = state;
+  const { blogType, date } = useParams<Params>();
+
+  const allPosts = useContext(PostsContext);
+  const { markdown, title } = allPosts?.[blogType]?.[date] || {};
 
   return (
-    <>
-      <h1>{title}</h1>
-      <StyledArticle>
-        <StyledH4>{date}</StyledH4>
-        <Markdown
-          options={{
-            overrides: {
-              a: { component: Anchor },
-              img: { component: Image },
-            },
-          }}
-        >
-          {markdown}
-        </Markdown>
-      </StyledArticle>
-    </>
+    title
+        ? (
+          <>
+            <h1>{title}</h1>
+            <StyledArticle>
+              <StyledH4>{date}</StyledH4>
+              <Markdown
+                options={{
+                  overrides: {
+                    a: { component: Anchor },
+                    img: { component: Image },
+                  },
+                }}
+              >
+                {markdown}
+              </Markdown>
+            </StyledArticle>
+          </>
+        )
+        : <>¯\_(ツ)_/¯</>
+      
   );
 };
 
