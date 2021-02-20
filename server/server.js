1d54742d7248c4560b91fe6e1ed4eb027837d0ea
nlemast <nlemaster47@gmail.com>
2021-02-20 16:29:34 -0500

    Update server get to send html file for all routes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-const express = require('express');
 const path = require('path');
 
-const app = express();
+const express = require('express');
 
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 const PORT = 3000;
 
-app.use('./dist', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/bundle.js'));
-});
+const app = express();
 
 app.use(express.static(DIST_DIR));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(HTML_FILE);
 });
 
