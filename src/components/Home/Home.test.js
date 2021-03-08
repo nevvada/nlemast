fbcd8d8f7609afdb02cd842f58b2092c13c97907
nlemast <nlemaster47@gmail.com>
2021-03-08 18:32:14 -0500

    Create simple unit test testing alt text of Img

---
+++ src/components/Home/Home.test.js
@@ -0,0 +1,15 @@
+import { cleanup, render } from '@testing-library/react';
+import React from 'react';
+
+import Home from './Home';
+
+describe('Home component', () => {
+  render(<Home />);
+  afterEach(cleanup);
+
+  it('should render the bongo cat img', () => {
+    const displayedImage = document.querySelector('img');
+
+    expect(displayedImage.alt).toContain('bongo cat');
+  });
+});
