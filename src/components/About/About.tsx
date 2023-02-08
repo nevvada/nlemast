4a844087607c5fde4ba0a7854bd7db768dd53588
nlemast <nlemaster47@gmail.com>
2023-02-07 17:09:05 -0800

    lol oops

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,8 +5,8 @@ import Anchor from '../Anchor/Anchor';
 const About: React.FC = () => (
   <>
     <p>
-      Hey, I&apos;m Nathan. I&apos;m a software engineer based out of Brooklyn, New York,
-      by way of California and Arkansas. I&apos;m most interested in futurism, urban planning,
+      Hey, I&apos;m Nevada. I&apos;m a software engineer based out of Seattle, Washington,
+      by way of California, Arkansas, and NYC. I&apos;m most interested in futurism, urban planning,
       history, and biking. I also like writing, so I&apos;ll probably throw up some things
       I&apos;ve written before, either about programming or random stuff I&apos;ve
       burned time learning about.
@@ -15,7 +15,7 @@ const About: React.FC = () => (
       You can find me on&nbsp;
       <Anchor href="https://github.com/nlemast">Github</Anchor>
       &nbsp;or&nbsp;
-      <Anchor href="https://www.linkedin.com/in/nathan-le-master/">LinkedIn</Anchor>
+      <Anchor href="https://www.linkedin.com/in/nevada-le-master/">LinkedIn</Anchor>
       .
     </p>
   </>
